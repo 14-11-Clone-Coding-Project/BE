d8c74dab5ff0a1ebb69f6ac02f0dd2f70a8e044d
been1118 <dsin1118@kakao.com>
2023-05-12 11:19:35 +0900

    init

---
+++ src/main/java/com/example/clone_be/CloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clone_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clone_be/CloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clone_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
